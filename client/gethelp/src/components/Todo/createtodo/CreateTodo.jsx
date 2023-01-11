a2076b2d00905b83eca9a5a4b9bc58e496913a20
dr3nan <dolgarrenan@gmail.com>
2023-01-11 20:11:56 +0100

    created handlSubmit function in CreateTodo component

---
+++ client/gethelp/src/components/Todo/createtodo/CreateTodo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { addTodo } from '../../../slices/TodoSlice'
+import { addToDo as addTodoAPI } from '../../../api/apiTodos';
+
+const handleSubmit = async (event, dispatch) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const todo = {
+    title: formData.get('title'),
+    date: formData.get('date'),
+    note: formData.get('note')
+  }
+  try {
+    const data = await addTodoAPI(todo);
+    dispatch(addTodo(data));
+  } catch (err) {
+    console.error(err);
+  }
+  event.target.reset();
+}
