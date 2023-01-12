a306242a6165949d099faf67f2f43e41523f38a8
dr3nan <dolgarrenan@gmail.com>
2023-01-12 09:46:06 +0100

    added TodoList component and styling files

--- client/gethelp/src/App.js
+++ client/gethelp/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CreateToDo from './components/Todo/createtodo/CreateTodo';
+import TodoList from './components/Todo/todolist/TodoList'
 
 function App() {
   return (
     <div className='App'>
       <CreateToDo />
+      <TodoList />
     </div>
   );
 }
