c5dc942dd33de3cc1fb661c0c0e27f1bd7595527
dr3nan <dolgarrenan@gmail.com>
2023-01-12 15:32:49 +0100

    added messageReducer to store

--- client/gethelp/src/store/store.js
+++ client/gethelp/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore, } from '@reduxjs/toolkit'
 import todoReducer from '../slices/TodoSlice'
 import tikcetReducer from '../slices/TicketSlice'
+import messageReducer from '../slices/MessageSlice'
 
 const store = configureStore({
   reducer: {
     todo: todoReducer,
-    tickets: tikcetReducer
+    tickets: tikcetReducer,
+    messages: messageReducer
   }
 });
 
