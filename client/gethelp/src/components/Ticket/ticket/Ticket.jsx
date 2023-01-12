08d0b217e60d1a94bbefea3149824564dc93e2bb
dr3nan <dolgarrenan@gmail.com>
2023-01-12 11:33:12 +0100

    added initial logic for Ticket component

--- client/gethelp/src/components/Ticket/ticket/Ticket.jsx
+++ client/gethelp/src/components/Ticket/ticket/Ticket.jsx
@@ -1,4 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { editTicket, deleteTicket } from '../../../slices/TicketSlice';
 import { editTicket as updateTicketAPI, deleteTicket as deleteTicketAPI } from '../../../api/apiTickets';
+
+const Ticket = ({ ticket }) => {
+  const dispatch = useDispatch();
+  const [isEditable, setIsEditable] = useState(false);
+  const [title, setTitle] = useState(ticket.title);
+  const [message, setMessage] = useState(ticket.message);
+
+  const handleEdit = async () => {
+    setIsEditable(!isEditable)
+    if (isEditable) {
+      try {
+        const updatedTicket = { title, message };
+        await updateTicketAPI(ticket._id, updatedTicket);
+        dispatch(editTicket(updatedTicket));
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+
+  const handleDelete = async (ticket) => {
+    try {
+      await deleteTicketAPI(ticket._id);
+      dispatch(deleteTicket(ticket));
+    } catch (err) {
+      console.error(err)
+    }
+  };
+
+  
+};
