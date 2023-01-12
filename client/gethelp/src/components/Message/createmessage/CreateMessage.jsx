7db81058b0f5c722df052fa3b79cbf1da9949815
dr3nan <dolgarrenan@gmail.com>
2023-01-12 15:21:08 +0100

    added initial CreateMessage logic and render logic

---
+++ client/gethelp/src/components/Message/createmessage/CreateMessage.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addMessage, resetMessage } from '../../../slices/MessageSlice';
+import { addMessage as addMessageAPI } from '../../../api/apiMessages';
+
+const handleSubmit = async (event, dispatch) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const message = {
+    message: formData.get('message'),
+    sender: 'admin'
+  }
+  try {
+    const data = await addMessageAPI(message);
+    dispatch(addMessage(data));
+  } catch (err) {
+    console.error(err);
+  }
+  event.target.reset();
+};
+
+const CreateMessage = () => {
+  const dispatch = useDispatch();
+
+  const handleReset = () => {
+    dispatch(resetMessage);
+  }
+  return (
+    <div className='create-message'>
+      <form onSubmit={event => handleSubmit(event, dispatch)}>
+        <div className='fields-create'>
+          <label htmlFor='message'>Message</label>
+          <input
+            type='text'
+            name='message'
+            id='message'
+          />
+        </div>
+        <div className='create-buttons'>
+          <button type='submit'>Send</button>
+          <button type='reset' onClick={handleReset}>Reset</button>
+        </div>
+      </form>
+    </div>
+  )
+};
+
+export default CreateMessage;
