1d4beacbc2b9ffae698cb04c5121a403781bce94
dr3nan <dolgarrenan@gmail.com>
2023-01-13 15:20:53 +0100

    fix: syntax cleanup

--- client/gethelp/src/components/Message/createmessage/CreateMessage.jsx
+++ client/gethelp/src/components/Message/createmessage/CreateMessage.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { resetMessage } from '../../../slices/MessageSlice';
 import { addMessage as addMessageAPI } from '../../../api/apiMessages';
-import { addMessageToTicket } from '../../../slices/ActiveTicketSlice';
 import { activeTicket as setActiveTicket } from '../../../slices/ActiveTicketSlice'
-
 
 const CreateMessage = () => {
   const dispatch = useDispatch();
   // we are receiving the active ticket from the reducer activeTicket (state), in the ActiveTicketSlice
+  // we will receive the whole ticket by being in the ticket itself
   const activeTicket = useSelector((state) => state.activeTicket);
-  console.log('active ticket', activeTicket.messages);
+  console.log('active ticket', activeTicket);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // we set what we are going to get from the form, to be sent later
+    // we set what we are going to get from the form to be sent later
     const formData = new FormData(event.target);
-    // we receive the message from the message input, hard code the user nd get today's date (not working date)
+    // we receive the message from the message input, hard code the user nd get today's date
     const message = {
       message: formData.get('message'),
       sender: 'admin',
@@ -24,11 +23,9 @@ const CreateMessage = () => {
     }
     try {
       // we receive the current ticket thanks to the call from state which includes the whole current ticket made above
-      // with its id. and we pass it to the api that creates messages (something is off here)
+      // with its id. and we pass it to the api that creates messages, we also add the inputs from the form
       const ticket = await addMessageAPI(activeTicket._id, message);
-      // dispatch should add the new message to the addMessageToTicket reducer in the ActiveTicketSlice, not the whole ticket
-      // the whole ticket is added to state in the Ticket component through the handleShowMessage function
-      // that is what we pass the the activeTicket reducer in the ActiveTicketSlice
+      // dispatch sends to the activeTicket action the ticket var that has all the info we need and updates the ticket
       dispatch(setActiveTicket(ticket));
       console.log('message added to ticket');
     } catch (err) {
